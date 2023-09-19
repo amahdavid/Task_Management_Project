4699733f58c13995939469f912765c9a18f6c99f
amahdavid <amahdavid34@gmail.com>
2023-09-18 21:30:56 -0700

    added a navbar and create button to homepage and options

--- task_management_app/src/components/homepage.js
+++ task_management_app/src/components/homepage.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React from 'react';
+import './homepage.css'; // Import your CSS file for styling
 
-const Homepage = () => {
-    return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
-    );
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <nav className="navbar">
+        <button className="create-button">Create +</button>
+      </nav>
+      <div className="content-container">
+        <aside className="sidebar">
+          <ul>
+            <li>Boards</li>
+            <li>Members</li>
+            <li>Calendar</li>
+            <li>Settings</li>
+          </ul>
+        </aside>
+        <main className="main-content">
+          <div className="content-header">
+            <h1>Boards</h1>
+          </div>
+          <div className="content-body">
+            {/* You can display your boards here */}
+            <p>No boards created</p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
-export default Homepage;
+export default HomePage;
